50c7a6972680d4e3f56d5bf8324f444ce474113d
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2025-02-15 04:47:01 +0200

    updated footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ export default function Footer() {
           <input
             type="email"
             id="app-download"
-            className="ms-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-5 p-2.5"
+            className="outline-green-400 transition-all ms-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-5 p-2.5"
             placeholder="Email..."
             required
           />
