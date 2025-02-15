d6b4f40b8f15a221b13742920e13505f4d50547b
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2025-02-15 07:53:40 +0200

    done loading screen and cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProductCard from "../ProductCard/ProductCard";
+import { GridLoader } from "react-spinners";
 
 export default function Home() {
   let [allProducts, setAllProducts] = useState([]);
+  let [loading, setLoading] = useState(false);
 
   async function getAllProducts() {
+    setLoading(true);
     const res = await axios.get(
       "https://ecommerce.routemisr.com/api/v1/products"
     );
     setAllProducts(res.data.data);
+    setLoading(false);
   }
   useEffect(() => {
     getAllProducts();
@@ -17,11 +21,19 @@ export default function Home() {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {allProducts.map((prod) => (
-          <ProductCard product={prod} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="w-full bg-green-400 text-white font-medium capitalize text-2xl h-screen flex justify-center items-center">
+          <GridLoader color="white"/>
+        </div>
+      ) : (
+        <div className="container mx-auto p-4">
+          <div className="grid gap-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+            {allProducts.map((prod) => (
+              <ProductCard product={prod} key={prod._id} />
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 
 export default function ProductCard(props) {
-  const { title, imageCover, price, _id, description, ratingAverage } =
-    props.product;
+  const {
+    title,
+    imageCover,
+    price,
+    _id,
+    description,
+    ratingsAverage,
+    category,
+  } = props.product;
 
   return (
     <>
-      <div className=" bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <div className=" bg-white rounded-lg drop-shadow-lg hover:drop-shadow-2xl hover: transition-all border-gray-200">
         <a href="#">
           <img
-            className="p-8 rounded-t-lg"
+            className="rounded-t-lg my-2"
             src={imageCover}
             alt="product image"
           />
         </a>
-        <div className="px-5 pb-5">
+
+        <div className="px-3 pb-5">
+          <span className="text-green-400 text-sm font-medium">
+            {category.name}
+          </span>
+
           <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              {title}
-            </h5>
+            <h5 className="font-semibold text-gray-700 ">{title}</h5>
           </a>
-          <div className="flex items-center mt-2.5 mb-5">
-            <div className="flex items-center space-x-1 rtl:space-x-reverse">
+
+          <div className="flex justify-between items-center pt-2">
+            <span className="text-gray-400">{price} EGP</span>
+
+            <div className="flex items-center">
               <svg
                 className="w-4 h-4 text-yellow-300"
                 aria-hidden="true"
@@ -31,57 +44,9 @@ export default function ProductCard(props) {
               >
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
               </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-gray-200 dark:text-gray-600"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
+
+              <span className="text-gray-400 mx-1">{ratingsAverage}</span>
             </div>
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">
-              5.0
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              ${price}
-            </span>
-            <a
-              href="#"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Add to cart
-            </a>
           </div>
         </div>
       </div>
