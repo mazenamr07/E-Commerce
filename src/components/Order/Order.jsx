f7781ae2983db105e56e804b65d4ca1165aa03ca
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2025-02-17 20:23:08 +0200

    log

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -44,13 +44,15 @@ export default function Order() {
   async function payVisa(values) {
     try {
       const res = await axios.post(
-        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartID}?url=E-Commerce/#/${window.location.origin}`,
+        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartID}?url=${window.location.origin}`,
         values,
         {
           headers: { token: localStorage.getItem("token") },
         }
       );
       window.open(res.data.session.url, "_blank");
+      console.log(res);
+      
     } catch (err) {
       if (cartID == undefined) {
         toast.error("Oops.. your cart is empty!");
